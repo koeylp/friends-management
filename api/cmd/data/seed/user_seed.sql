89c1fa3d9183c6eb384757f69fa68aa3c26cf87b
khoi.le <thekhoi.k@gmail.com>
2024-10-28 09:33:31 +0700

    add seed_user

---
+++ api/cmd/data/seed/user_seed.sql
@@ -0,0 +1,33 @@
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('676330d0-71c8-4f9d-ac58-e83f9808241c'::uuid, 'john@example.com', '2024-10-28 02:12:36.121', '2024-10-28 02:12:36.121');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('6369c7ae-03cc-4194-a096-532888cdcf3f'::uuid, 'doe@example.com', '2024-10-28 02:13:33.187', '2024-10-28 02:13:33.187');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('3bac093e-8075-4fbf-b0f4-3d5a87d72a2e'::uuid, 'alex@example.com', '2024-10-28 02:13:37.193', '2024-10-28 02:13:37.193');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('1cd0d10b-1912-49d9-a4bd-c68e5ee4b7ef'::uuid, 'peter@example.com', '2024-10-28 02:13:42.374', '2024-10-28 02:13:42.374');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('923fed8e-d002-43b4-8955-8c0a21ee72ac'::uuid, 'shelby@example.com', '2024-10-28 02:13:49.493', '2024-10-28 02:13:49.493');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('e50d9728-ab63-475a-a401-8ef5f1844536'::uuid, 'ross@example.com', '2024-10-28 02:13:55.393', '2024-10-28 02:13:55.393');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('2a4f5dbf-abad-40e7-b320-0512b408b09a'::uuid, 'joey@example.com', '2024-10-28 02:14:03.701', '2024-10-28 02:14:03.701');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('28dbacd1-d805-4484-bb93-c060ccf7061a'::uuid, 'monica@example.com', '2024-10-28 02:14:08.420', '2024-10-28 02:14:08.420');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('106855f2-6270-4d93-bfb6-b410592c9f07'::uuid, 'rachel@example.com', '2024-10-28 02:14:13.189', '2024-10-28 02:14:13.189');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('49a12f65-f67f-4997-89ed-fd1849b1dbff'::uuid, 'phoebe@example.com', '2024-10-28 02:14:21.891', '2024-10-28 02:14:21.891');
+INSERT INTO public.users
+(id, email, created_at, updated_at)
+VALUES('8fd3f13a-109d-4944-a90e-27d64307fabb'::uuid, 'phill@example.com', '2024-10-28 02:14:31.541', '2024-10-28 02:14:31.541');
